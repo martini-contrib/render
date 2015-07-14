776e9fcd4954d9d6e0f125cfd02d59c58df0ab27
Jie Fan <ustc.flyingfox@gmail.com>
2015-07-13 22:16:33 -0400

    Fix indents.

--- render.go
+++ render.go
@@ -41,7 +41,7 @@ import (
 	"encoding/xml"
 	"fmt"
 	"html/template"
-  "io"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -49,7 +49,6 @@ import (
 	"strings"
 
 	"github.com/oxtoacart/bpool"
-
 	"github.com/go-martini/martini"
 )
 
